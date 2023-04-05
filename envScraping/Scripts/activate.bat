9362a2386a1663fc1d15117118092dda6568398b
Pavel <borisov.p.92@yandex.ru>
2023-04-05 09:13:42 +0300

    commit before merge

--- envScraping/Scripts/activate.bat
+++ envScraping/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\programming\python\freelance\cmc_parse\envScraping
+set VIRTUAL_ENV=E:\programms\job_projects\project\cmc_scraping\envScraping
 
 if not defined PROMPT set PROMPT=$P$G
 
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import json
+import time
 
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
 import csv
 
 
@@ -18,7 +24,7 @@ class ParseHtml:
     def get_html(self):
         """Возвращает html-страницу"""
         req = requests.get(self.url, headers=self.header)
-
+        
         if req.text:
             return req.text
 
@@ -26,13 +32,13 @@ class ParseHtml:
 
 
 class Sensors:
-    def __init__(self):
-        self.url = 'https://sensors.saasexch.com/sa.gif?project=cmc'
+    def __init__(self, url):
+        self.url = url
 
     @property
     def header(self):
         return {
-            'accept': '*/*',
+            'Referer': 'https://s2.coinmarketcap.com/',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.1.806 Yowser/2.5 Safari/537.36'
         }
 
@@ -43,6 +49,23 @@ class Sensors:
             return req.text
 
         return False
+    
+
+def get_dynamic_data(url):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+
+    driver = webdriver.Chrome(
+        executable_path='chromedriver/chromedriver',
+        options=chrome_options,
+    )
+
+    driver.get(url=url)
+    time.sleep(3)
+
+    print(driver.find_element(By.CLASS_NAME, 'dropdownItem').text)
+
+    return driver.close()
 
 
 if __name__ == '__main__':
@@ -61,26 +84,38 @@ if __name__ == '__main__':
 
         # Сбор ссылок на ресурсы
         bs = BeautifulSoup(html_cmc, 'lxml')
-        cmc_links = bs.find('table').find_all('a', class_='cmc-link')
+        cmc_table_tr = bs.find(class_='cmc-table').find('tbody').find_all('tr')
 
         links_to_src = []
 
-        for link in cmc_links:
+        for tr in cmc_table_tr:
+            link = tr.find('a', class_='cmc-link')
             url_link = cmc_domen + link['href']
             links_to_src.append(url_link)
 
         count = 0
 
         for link in links_to_src:
-            if count == 0:
+            if count == 2:
+                break
+            else:
                 src_page = ParseHtml(link)
                 html_src = src_page.get_html()
 
+                # with open(f'data/{count}_link.html', 'w', encoding='utf-8') as file:
+                #     file.write(html_src)
+
                 bs_link = BeautifulSoup(html_src, 'lxml')
-                src_div = bs_link.find(class_='nameSection')
+                
+                src_name_header = bs_link.find(class_='nameSection').find(class_='nameHeader')
 
-                src_title = src_div.find(class_='sc-1d5226ca-0')
+                src_title = src_name_header.find(class_='sc-1d5226ca-0').text
 
-                print(src_title.text)
+                # src_links_section = bs_link.find(class_='linksSection').find(class_='content').find_all('li')
+
+                get_dynamic_data(link)
+
+                
+
 
             count += 1
